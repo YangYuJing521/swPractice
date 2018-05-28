033f945ff5dfb6a93c259ce4fdefecd59684014c
YangYuJing521 <475201052@qq.com>
2018-05-28 11:09:20 +0800

    定义变量与数据推导

--- swPractice/swPractice/ViewController.swift
+++ swPractice/swPractice/ViewController.swift
@@ -12,14 +12,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        basicDataType()
+        
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+//    定义变量与数据推导
+    func basicDataType() {
+        //let修饰不可变常量和对像，存放在栈中，指向堆中的对象 是指向堆的指针，对象可变指针不可变
+        let view:UIView = UIView()
+        view.backgroundColor = UIColor.red
+        //var修饰的常量可变,指向对象的指针也可变 Int = OC NSInteger
+        var num:Int  = 6
+        num = 7
+        print(num)
+        
+        // 类型推导与数据转换
+        let a = 4
+        let b = 5.5
+        let result = Double(a) + b
+        print(result);
 
+    }
 
 }
 
